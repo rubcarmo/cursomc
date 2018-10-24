a29a8f0df8157b7995c063ba5c9772a2794b4d62
Rubens Jesus <jesus.rubens@gmail.com>
2018-10-24 12:20:23 -0300

    Validacao customizada email nao repetido na atualizacao de cliente

--- src/main/java/com/rjesus/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/rjesus/cursomc/dto/ClienteDTO.java
@@ -8,8 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.rjesus.cursomc.domain.Cliente;
+import com.rjesus.cursomc.services.validation.ClienteUpdate;
 
-
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
